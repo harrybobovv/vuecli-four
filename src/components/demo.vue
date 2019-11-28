1448998a45bb79c96c7deca7a2f73133f78ee678
闻家奇 <939949243@qq.com>
2019-11-28 20:45:34 +0800

    git

---
+++ src/components/demo.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="">
+        add 新文件加入暂存区
+        commit 提交到本地仓库
+        push 提交到远程仓库
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "",
+        components: {},
+        data() {
+            return {}
+        },
+        methods: {}
+    }
+</script>
+
+<style scoped>
+
+</style>
